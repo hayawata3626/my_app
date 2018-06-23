7617440e1d21a17020f35f683f6893da0d5c3b9b
hayawata3626 <top.men09@gmail.com>
2018-06-23 16:23:42 +0900

    ログインフォーム用のコンポーネントを作成

---
+++ frontend/src/component/users/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="loginform">
+    <div class='container'>
+      <h1>ログイン</h1>
+      <form action='/login' method="post">
+        <p>メールアドレス</p>
+        <input type='text' name="email" :value="email">
+        <p>パスワード</p>
+        <input type='password' name="password" :value="password">
+        <input type='submit' value="ログイン">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+export default class LoginForm extends Vue {
+  @Prop()
+  public email: string;
+
+  @Prop()
+  public password: string;
+}
+</script>
