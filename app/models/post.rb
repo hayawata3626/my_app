e409967ed1635318fb34e006936d1fcf422dcc35
hayawata3626 <top.men09@gmail.com>
2018-07-18 09:27:39 +0900

    タグ追加用のメソッドを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -22,4 +22,16 @@ class Post < ApplicationRecord
     end
   end
 
+  def save_posts(savepost_tags)
+    current_tags = self.tags.pluck(:name) unless self.tags.nil?
+    old_tags = current_tags - savepost_tags
+    new_tags = savepost_tags - current_tags
+
+    # Create new taggings:
+    new_tags.each do |new_name|
+      post_tag = Tag.find_or_create_by(name:new_name)
+      self.tags << post_tag
+    end
+  end
+
 end
