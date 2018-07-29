958d40bb5a1c41db7c6f8ae9b7e925b35004ae60
hayawata3626 <top.men09@gmail.com>
2018-07-29 13:17:10 +0900

    スタイルの追加

--- frontend/src/component/layouts/Header.vue
+++ frontend/src/component/layouts/Header.vue
@@ -30,6 +30,71 @@ export default class Header extends Vue {
 </script>
 
 <style>
+.header {
+  width: 100%;
+  top: 0;
+  padding: 10px 0;
+  background: #323232;
+}
+.header_menu {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.header_menu_inner {
+  display: flex;
+  align-items: center;
+}
+.user_info {
+  display: flex;
+  align-items: center;
+}
+.login_menu {
+  display: flex;
+  justify-content: space-between;
+}
 
+.login_menu > li {
+  padding: 0 7px
+}
+
+.login_menu > li > a {
+  text-decoration: none;
+  color: #fff;
+  font-weight: lighter;
+  font-size: 14px;
+}
+.user_img {
+  max-width: 40px;
+  max-height: 40px;
+  border-radius: 50%;
+  background: #fff;
+}
+
+.user_img > img {
+  width: 100%;
+}
+
+.searchForm_content {
+  position: relative;
+}
+.searchForm_content input[type="text"] {
+  padding: 10px;
+  border-radius: 15px;
+  border: none;
+  padding-left: 40px;
+  font-weight: lighter;
+  font-size: 12px;
+}
+.searchForm_content input[type="submit"] {
+   visibility: hidden;
+}
+
+.searchIcon {
+  position: absolute;
+  left: 5%;
+  top: 50%;
+  transform: translateY(-50%);
+ }
 </style>
 
